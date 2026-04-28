e193dee58bd8cc35b0f4ac85e384515de8371e5c
20156183 <20156183@tafe.wa.edu.au>
2026-04-28 19:39:40 +0800

    base code framing initialize

---
+++ wordle.py
@@ -0,0 +1,36 @@
+# Semi-Wordle
+#
+# Author: Benz Lenard Culanggo
+# Student ID: 20156183
+#
+# Course: Certificate IV Programming
+# Lecturer: Para O'Kelly
+
+# TODO:
+#Repiclate wordle mechanics using learned programming
+
+# Variables and Constants
+# TODO: Define Constants
+
+# TODO: Define Variables 
+
+# Application Functions
+# TODO: Score Guess Function
+
+# TODO: Read File Into Word List Function
+
+# TODO: Display Greeting Function
+def show_greeting():
+    print("Welcome")
+
+# TODO: Display Instructions Function
+def show_instructions():
+    print("Instructions")
+
+# TODO: Any Optional Additional Functions 
+
+# TODO: Play Game Function
+
+#TODO: Testing Function
+
+#TODO: Main Program
